4a53ccc12a20a6737677b104c6b25c9760a6ccdd
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-01-26 15:05:58 +0500

    Created a student model and migrated.

--- studentApp/models.py
+++ studentApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    studentID = models.PositiveSmallIntegerField(primary_key=True)
+    firstName = models.TextField(max_length=15, default="Enter first name... ")
+    lastName = models.TextField(max_length=15, default="Enter last name... ")
+    studentEmail = models.EmailField(blank=True)
+    studentAge = models.IntegerField(default="Enter student age... ")
+    studentGrade = models.IntegerField(default="Enter student grade... ")
