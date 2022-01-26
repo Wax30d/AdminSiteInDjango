35d0836c7fedc034a79456ae7dd05cc174bf2bfa
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-01-26 15:36:34 +0500

    View function is added.

--- studentApp/views.py
+++ studentApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def student_show(request):
+    x = []
+    for i in range(10):
+        x.append(i)
+
+    return HttpResponse(f"<h1>Django Tutorials by Wax30d</h1>\nThe Digits are {x}")
